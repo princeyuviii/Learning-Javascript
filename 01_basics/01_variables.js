0ee1e4f1f0bd2483ba3729eb61172a3c3c7d6b64
Yuvraj singh rathore <yuvisinghrathore678678@gmail.com>
2024-07-16 11:15:24 +0000

    02_dataTypes Created

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -5,6 +5,10 @@ accountCity = "Jaipur"
 let accountState;
 
 //accountId = 7777777
+/*
+Prefer not to use var
+because of issue in block scope and functional scope 
+*/
 
 accountEmail = "yuvraj@google.com"
 accountPassword = "1234"
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,4 @@
+"use strict"; 
+//treats code as newer version (automatically included by default most of the times)
+
+//alert(3 + 3) Using Node.js not browser
