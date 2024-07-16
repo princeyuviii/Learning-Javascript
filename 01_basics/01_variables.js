f3f487ff34ed973be3a85edd6d8ffe6a7b0cb261
Yuvraj singh rathore <yuvisinghrathore678678@gmail.com>
2024-07-16 11:00:30 +0000

    Let,Const and Var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 12345
+let accountEmail = "yuvi@gmail.com"
+var accountPassword = "6969"
+accountCity = "Jaipur"
+let accountState;
+
+//accountId = 7777777
+
+accountEmail = "yuvraj@google.com"
+accountPassword = "1234"
+accountCity = "Pune"
+
+console.table([accountId , accountEmail , accountPassword , accountCity , accountState])
+console.log(accountId);
